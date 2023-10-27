eb469193f4adad857e23d3998fa67e2ac074b58d
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-10-27 17:26:14 +0300

    feat: add sass as dev dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./styles.scss";
 
 export default class App extends React.Component {
   render() {
-    return <div>Hello, World!</div>;
+    return <div className="hello">Hello, World!</div>;
   }
 }
