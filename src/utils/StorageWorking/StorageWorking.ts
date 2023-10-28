6d3cac9c95a3ab7d235b215811d8cbe218cc4ed0
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-10-28 13:57:58 +0300

    feat: add component for working with local storage

---
+++ src/utils/StorageWorking/StorageWorking.ts
@@ -0,0 +1,9 @@
+const STORAGE_FIELD_FOR_QUERY = 'query';
+
+export function getQueryFromStorage(): string | null {
+  return localStorage.getItem(STORAGE_FIELD_FOR_QUERY);
+}
+
+export function setQueryToStorage(query: string): void {
+  localStorage.setItem(STORAGE_FIELD_FOR_QUERY, query);
+}
