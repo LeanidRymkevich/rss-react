7a0d10e0ff14e03704fea0f2687c600dc71c7c29
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-11-13 23:20:09 +0300

    refactor: move stubs for NewPageUtils tests to __mock__ folder

--- src/utils/NewsPageUtils.test.ts
+++ src/utils/NewsPageUtils.test.ts
@@ -1,172 +1,23 @@
-import { INewsContext } from 'src/pages/News/types';
+import {
+  mockInput_1,
+  mockInput_2,
+  mockOutput_1,
+  mockOutput_2,
+} from 'src/__mocks__/NewsPageUtils';
 import {
   getNewsItemProps,
   calcPageAmount,
   createDigitsArray,
   setNewsRecords,
 } from 'src/utils/NewsPageUtils';
-import { NewsItemProps } from 'src/components/NewsItem/types';
-
-const testContext: INewsContext = {
-  query: '',
-  total: 2,
-  page: '1',
-  limit: '10',
-  articles: [
-    {
-      source: {
-        id: 'wired',
-        name: 'Wired',
-      },
-      author: 'Gideon Lichfield, Lauren Goode',
-      title: 'Where Does Crypto Go From Here?',
-      description:
-        'We talk with Michael Casey, the chief content officer of CoinDesk, almost one year after the news site brought down Sam Bankman-Fried’s cryptocurrency empire FTX.',
-      url: 'https://www.wired.com/story/have-a-nice-future-podcast-24/',
-      urlToImage:
-        'https://media.wired.com/photos/6525c8ac419624284be05210/191:100/w_1280,c_limit/HANF-Michael%20Casey.jpg',
-      publishedAt: '2023-10-11T11:00:00Z',
-      content:
-        "Gideon Lichfield: You said just now that what Sam Bankman-Fried did was allegedly illegal. Is there a world in which it wasn't illegal?\r\nMichael Casey: I think we allI know. I'm trying to be a good j… [+2082 chars]",
-    },
-    {
-      source: {
-        id: null,
-        name: 'Gizmodo.com',
-      },
-      author: 'Maxwell Zeff',
-      title: 'Crypto Exchanges, Not Just FTX, Are All a Mess Right Now',
-      description:
-        'The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes on Binance declined, according to a Bloomberg report Friday.…',
-      url: 'https://gizmodo.com/crypto-exchanges-ftx-binance-genesis-gemini-are-a-mess-1850968083',
-      urlToImage:
-        'https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/46326bbf3c33c4ddb3f18069c82167d7.jpg',
-      publishedAt: '2023-10-27T20:10:00Z',
-      content:
-        'The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes … [+1852 chars]',
-    },
-  ],
-};
-
-const testContext_1: INewsContext = {
-  query: '',
-  total: 2,
-  page: '1',
-  limit: '10',
-  articles: [
-    {
-      source: {
-        id: 'wired',
-        name: 'Wired',
-      },
-      author: 'Gideon Lichfield, Lauren Goode',
-      title: 'Where Does Crypto Go From Here?',
-      description:
-        'We talk with Michael Casey, the chief content officer of CoinDesk, almost one year after the news site brought down Sam Bankman-Fried’s cryptocurrency empire FTX.',
-      url: 'https://www.wired.com/story/have-a-nice-future-podcast-24/',
-      urlToImage:
-        'https://media.wired.com/photos/6525c8ac419624284be05210/191:100/w_1280,c_limit/HANF-Michael%20Casey.jpg',
-      publishedAt: '2023-10-11T11:00:00Z',
-      content:
-        "Gideon Lichfield: You said just now that what Sam Bankman-Fried did was allegedly illegal. Is there a world in which it wasn't illegal?\r\nMichael Casey: I think we allI know. I'm trying to be a good j… [+2082 chars]",
-    },
-    {
-      source: {
-        id: null,
-        name: 'Gizmodo.com',
-      },
-      author: 'Maxwell Zeff',
-      title: 'Crypto Exchanges, Not Just FTX, Are All a Mess Right Now',
-      description:
-        'The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes on Binance declined, according to a Bloomberg report Friday.…',
-      url: 'https://gizmodo.com/crypto-exchanges-ftx-binance-genesis-gemini-are-a-mess-1850968083',
-      urlToImage:
-        'https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/46326bbf3c33c4ddb3f18069c82167d7.jpg',
-      publishedAt: '2023-10-27T20:10:00Z',
-      content:
-        'The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes … [+1852 chars]',
-    },
-  ],
-};
-
-const returnedValue_1: NewsItemProps[] = [
-  {
-    itemNum: '1',
-    description:
-      'We talk with Michael Casey, the chief content officer of CoinDesk, almost one year after the news site brought down Sam Bankman-Fried’s cryptocurrency empire FTX.',
-    publisher: testContext.articles[0].source.name,
-    author: 'Gideon Lichfield, Lauren Goode',
-  },
-  {
-    itemNum: '2',
-    description:
-      'The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes on Binance declined, according to a Bloomberg report Friday.…',
-    publisher: testContext.articles[1].source.name,
-    author: 'Maxwell Zeff',
-  },
-];
-
-const testContext_2: INewsContext = {
-  query: '',
-  total: 2,
-  page: '1',
-  limit: '10',
-  articles: [
-    {
-      source: {
-        id: 'wired',
-        name: 'Wired',
-      },
-      author: null,
-      title: 'Where Does Crypto Go From Here?',
-      description: null,
-      url: 'https://www.wired.com/story/have-a-nice-future-podcast-24/',
-      urlToImage:
-        'https://media.wired.com/photos/6525c8ac419624284be05210/191:100/w_1280,c_limit/HANF-Michael%20Casey.jpg',
-      publishedAt: '2023-10-11T11:00:00Z',
-      content:
-        "Gideon Lichfield: You said just now that what Sam Bankman-Fried did was allegedly illegal. Is there a world in which it wasn't illegal?\r\nMichael Casey: I think we allI know. I'm trying to be a good j… [+2082 chars]",
-    },
-    {
-      source: {
-        id: null,
-        name: 'Gizmodo.com',
-      },
-      author: null,
-      title: 'Crypto Exchanges, Not Just FTX, Are All a Mess Right Now',
-      description: null,
-      url: 'https://gizmodo.com/crypto-exchanges-ftx-binance-genesis-gemini-are-a-mess-1850968083',
-      urlToImage:
-        'https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/46326bbf3c33c4ddb3f18069c82167d7.jpg',
-      publishedAt: '2023-10-27T20:10:00Z',
-      content:
-        'The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes … [+1852 chars]',
-    },
-  ],
-};
-
-const returnedValue_2: NewsItemProps[] = [
-  {
-    itemNum: '1',
-    description: 'none',
-    publisher: testContext.articles[0].source.name,
-    author: 'none',
-  },
-  {
-    itemNum: '2',
-    description: 'none',
-    publisher: testContext.articles[1].source.name,
-    author: 'none',
-  },
-];
 
 describe('Tests for getNewsItemProps function', (): void => {
   test('it returns appropriate values', (): void => {
-    expect(getNewsItemProps(testContext_1)).toEqual(returnedValue_1);
+    expect(getNewsItemProps(mockInput_1)).toEqual(mockOutput_1);
   });
 
   test('it returns "none" string for null properties', (): void => {
-    expect(getNewsItemProps(testContext_2)).toEqual(returnedValue_2);
+    expect(getNewsItemProps(mockInput_2)).toEqual(mockOutput_2);
   });
 });
 
